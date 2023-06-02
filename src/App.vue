2f8519622890835429c8db94734c8aeb3d2dc7f0
viferraro <viferraro@edu.unirio.br>
2023-06-02 16:35:28 -0300

    imagens

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <v-app id="inspire">
     <v-app-bar flat>
       <v-container class="fill-height d-flex align-center">
+       <v-img
+  max-height="50"
+  max-width="50"
+  src="C:\TrelloJPEricVivi\pjvue\src\assets\treco.png"
+></v-img>
         <v-avatar class="me-10 ms-4" color="grey-darken-1" size="32"></v-avatar>
 
         <v-btn v-for="link in links" :key="link" variant="text">
