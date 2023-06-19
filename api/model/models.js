a1b1e9df74cb26e4b1827fdc1e719cbf7e32694e
Joao <joaoprf2001@gmail.com>
2023-06-19 16:28:44 -0300

    teste

--- api/model/models.js
+++ api/model/models.js
@@ -15,7 +15,10 @@ var Usuario = null;
 // Variavel global que mantém o modelo dos quadros
 //
 var Quadro = null;
-
+//
+// Variavel global que mantém o modelo das listas
+//
+var Lista = null;
 //
 // Variavel global que mantém o modelo dos cards
 //
