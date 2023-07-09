d1240dab7af4a2dfa968a6f30b361364ef3ef4e4
viferraro <viferraro@edu.unirio.br>
2023-07-09 17:48:42 -0300

    commitando

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-navigation-drawer v-if="$root.credentials" app v-model="drawer">
-        <v-list nav>
-            <v-list-item-group active-class="deep-green--text text--accent-4">
-                <v-list-item>
-                    <v-list-item-title @click="homeQuadros">Quadros</v-list-item-title>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-title @click="homeColecao">Coleção</v-list-item-title>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-title @click="alteraRegistro">Configuração da conta</v-list-item-title>
-                </v-list-item>
-            </v-list-item-group>
-        </v-list>
+      <v-list nav>
+        <v-list-item-group active-class="deep-green--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title @click="homeQuadros">Quadros</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title @click="homeColecao">Coleção</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title @click="alteraRegistro">Configuração da conta</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app color="#BED9D9">
@@ -27,9 +27,16 @@
         <span v-if="$root.credentials">
           Olá, {{ $root.credentials.nome }}.
           <router-link :to="{ name: 'change-password' }" replace>
-           
+
           </router-link>
-          <v-btn icon @click="logout"><v-icon>mdi-logout</v-icon></v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on" @click="logout">
+                <v-icon>mdi-logout</v-icon>
+              </v-btn>
+            </template>
+            <span>Sair</span>
+          </v-tooltip>
         </span>
 
       </v-container>
@@ -63,7 +70,7 @@ export default {
       this.$router.replace('/');
     },
 
-    homeQuadros: function() {
+    homeQuadros: function () {
       this.$router.replace('/quadros/');
     },
 
@@ -72,7 +79,7 @@ export default {
 
     },
 
-    homeColecao: function() {
+    homeColecao: function () {
       this.$router.replace('/colecoes/');
     },
   },
